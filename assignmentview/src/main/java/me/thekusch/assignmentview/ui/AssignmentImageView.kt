f3741ec0de3e6bc5b73107e4037f327a22eaac43
muratkus <muratkus.dev@gmail.com>
2022-04-30 17:54:46 +0300

    Add imageLoad and load time log

--- assignmentview/src/main/java/me/thekusch/assignmentview/ui/AssignmentImageView.kt
+++ assignmentview/src/main/java/me/thekusch/assignmentview/ui/AssignmentImageView.kt
@@ -1,8 +1,16 @@
 package me.thekusch.assignmentview.ui
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import me.thekusch.assignmentview.R
 import java.lang.Exception
 
@@ -10,8 +18,15 @@ internal class AssignmentImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.assignmentImageView
-): AppCompatImageView(context, attrs, defStyleAttr) {
+) : AppCompatImageView(context, attrs, defStyleAttr) {
 
+    private var _loadTime: Long = DEFAULT_LOAD_TIME
+
+    var loadTime: Long
+        get() = _loadTime
+        set(value) {
+            _loadTime = value
+        }
 
     init {
         obtainStyledAttributes(attrs, defStyleAttr)
@@ -32,5 +47,49 @@ internal class AssignmentImageView @JvmOverloads constructor(
         } finally {
             typedArray.recycle()
         }
+    }
+
+    fun loadResource(resourceUrl: String?) {
+        val startTime = System.currentTimeMillis()
+        Glide.with(context)
+            .load(resourceUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // no-op
+                    // load time wil remain same as DEFAULT_LOAD_TIME (-1)
+                    imageLoadLog(resourceUrl)
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    val resultTime = System.currentTimeMillis()
+                    loadTime = resultTime - startTime
+                    imageLoadLog(resourceUrl)
+                    return false
+                }
+            })
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .into(this)
+    }
+
+    private fun imageLoadLog(resource: String?) {
+        val status = if (loadTime == DEFAULT_LOAD_TIME) "FAIL" else "SUCCESS"
+        Log.d(TAG, "Status: $status --> resource: $resource trackedLoadTime: $loadTime")
+    }
+
+    companion object {
+        const val DEFAULT_LOAD_TIME = -1L
+        const val TAG = "AssignmentImageView"
     }
 }
